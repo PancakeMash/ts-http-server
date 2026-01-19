5a266a02bfb3d75f39781859b25127d20a7c812c
PancakeMash <arpandevofficial@gmail.com>
2026-01-19 21:30:21 +0000

    H: Added ability to add and verify JWTs for easier authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import argon2 from 'argon2';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { UnauthorisedError } from './middleware';
 
 export async function hashPassword(password: string): Promise<string> {
     
@@ -12,3 +14,30 @@ export async function checkPasswordHash(password: string, hash: string): Promise
     return await argon2.verify(hash, password);
 
 }
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    const payload: payload = {
+        iss: 'chirpy',
+        sub: userID,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + expiresIn,
+    };
+
+    return jwt.sign(payload, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    
+    let validatedJWT;
+    try {
+        validatedJWT = jwt.verify(tokenString, secret);
+    } catch (err) {
+        throw new UnauthorisedError(`Invalid token: ${err}`);
+    }
+
+    const payload = validatedJWT as JwtPayload;
+
+    return payload.sub as string;
+}
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
